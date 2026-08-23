21dcf0fa4279f9536ef8f66cdfa39a0dcf00ea6b
wildanhwing07-cmyk <wildanhwing07@gmail.com>
2026-08-23 14:30:54 +0700

    Update dashboard.py

--- dashboard.py
+++ dashboard.py
@@ -12,7 +12,7 @@ st.set_page_config(
 )
 
 # ==========================================
-# 1. LOAD DATA (Hanya membaca CSV biasa)
+# 1. LOAD DATA 
 # ==========================================
 @st.cache_data
 def load_data():
